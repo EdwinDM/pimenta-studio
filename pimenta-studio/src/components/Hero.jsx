10eec70d1890bbf6c850f686fbd5d0afb9564056
Edwin Delgado <edwindm2003@gmail.com>
2024-08-15 12:08:19 -0600

    Añadidos a las cards y modificaciones de la información interna del sitio
    Añadidos a las cards y modificaciones de la información interna del sitio

--- pimenta-studio/src/components/Hero.jsx
+++ pimenta-studio/src/components/Hero.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import { motion } from "framer-motion";
 
 export const Hero = () => {
-
   return (
     <section
-      className="w-screen h-screen flex justify-center items-center bg-bgDark1 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+      className="w-screen h-screen flex justify-center items-center bg-bgDark1 hero-bg-gradient"
       id="home"
     >
-      <div className="w-full md:w-[800px] xl:w-[900px] flex flex-col justify-center items-center text-center">
+      <div className="w-full flex flex-col justify-center items-center text-center px-4 md:px-20 lg:px-32">
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <h3 className="text-secondaryColor text-sm sm:text-base  mb-6 font-bold">
+          <h3 className="text-secondaryColor text-sm sm:text-base mb-6 font-bold">
             Descubre un nuevo mundo
           </h3>
         </motion.div>
@@ -23,10 +21,10 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.05 }}
         >
-          <div className="text-4xl sm:text-4xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-8 md:px-20 lg:px-4">
-            <h1>Desarollo Profesional</h1>
+          <div className="text-4xl sm:text-5xl lg:text-7xl font-bold tracking-wide text-primaryText">
+            <h1>Desarrollo Profesional</h1>
           </div>
-          <h1 className="text-3xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-primaryText  px-8 sm:px-20 md:px-24 lg:px-24">
+          <h1 className="text-3xl sm:text-5xl lg:text-7xl font-bold tracking-wide text-primaryText">
             para tu Negocio
           </h1>
         </motion.div>
@@ -35,8 +33,8 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.1 }}
         >
-          <h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            Nos especializamos en desarrollar formulas para que tu negocio crezca utilizando las más recientes novedades y tecnologías a tu favor, contamos con espacios personalizados.
+          <h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-8 sm:mt-10 px-8 lg:px-24 xl:px-36 max-w-6xl">
+            Nos especializamos en desarrollar fórmulas para que tu negocio crezca utilizando las más recientes novedades y tecnologías a tu favor. Contamos con espacios personalizados.
           </h2>
         </motion.div>
         <motion.div
@@ -44,16 +42,24 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.15 }}
         >
-          <div className="flex flex-col gap-2 sm:flex-row mt-14 justify-center">
-            <a href="https://api.whatsapp.com/send?phone=63536621&text=Hola,%20estoy%20interesado%20en%20sus%20servicios.%20Quiero%20más%20información." target="_blank">  
+          <div className="flex flex-col gap-2 sm:flex-row mt-10 sm:mt-12 justify-center">
+            <a
+              href="https://api.whatsapp.com/send?phone=63536621&text=Hola,%20estoy%20interesado%20en%20sus%20servicios.%20Quiero%20más%20información."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button
-                  className="contained-button w-64 sm:w-52 h-12 mr-0 sm:mr-4 lg:mr-6 mb-2 sm:mb-0"
-                  aria-label="Get started"
+                className="contained-button w-64 sm:w-52 h-12 mr-0 sm:mr-4 mb-2 sm:mb-0"
+                aria-label="Get started"
               >
                 Contáctanos
               </button>
             </a>
-            <button className="w-64 sm:w-52 h-12 rounded-xl font-bold text-primaryText border border-solid  flex justify-center items-center cursor-pointer bg-bgDark2 hover:bg-bgDark3 border-primaryColor transition" aria-label="Live demo" onClick={() => window.location.href = '/#pricing'}>
+            <button
+              className="w-64 sm:w-52 h-12 rounded-xl font-bold text-primaryText border border-solid flex justify-center items-center cursor-pointer bg-bgDark2 hover:bg-bgDark3 border-primaryColor transition"
+              aria-label="Live demo"
+              onClick={() => window.location.href = '/#pricing'}
+            >
               Crear planes
             </button>
           </div>
--- pimenta-studio/src/components/Pricing.jsx
+++ pimenta-studio/src/components/Pricing.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import pimenta_multimedia from "../assets/logos/pimenta-multimedia.svg";
+import pimenta_informatica from "../assets/logos/pimenta-informatica.svg";
 
-const optionsData = [
+const optionsData1 = [
   { text: "Diseño de Logotipo Premium con Guía de Uso", price: 100 },
-  { text: "Diseño de Tarjetas de Presentación y Papelería Corporativa", price: 80 },
+  { text: "Diseño de Tarjetas de Papelería Corporativa", price: 80 },
   { text: "Diseño de Catálogo o Brochure de 8 Páginas", price: 120 },
   { text: "Diseño de Seis Piezas Gráficas para Redes Sociales", price: 90 },
   { text: "Revisiones Ilimitadas y Consultoría en Branding", price: 110 }
 ];
 
-export const Pricing = () => {
-  const [selectedOptions, setSelectedOptions] = useState([]);
+const optionsData2 = [
+  { text: "Desarrollo de Página Web Corporativa", price: 150 },
+  { text: "Optimización SEO para Motores de Búsqueda", price: 100 },
+  { text: "Mantenimiento y Soporte Técnico Web", price: 120 },
+  { text: "Integración de Comercio Electrónico", price: 200 },
+  { text: "Administración de Páginas Web", price: 90 }
+];
 
-  const handleOptionChange = (option) => {
-    setSelectedOptions((prevState) =>
+export const Pricing = () => {
+  const [selectedOptions1, setSelectedOptions1] = useState([]);
+  const [selectedOptions2, setSelectedOptions2] = useState([]);
+
+  const handleOptionChange1 = (option) => {
+    setSelectedOptions1((prevState) =>
+      prevState.includes(option)
+        ? prevState.filter((item) => item !== option)
+        : [...prevState, option]
+    );
+  };
+
+  const handleOptionChange2 = (option) => {
+    setSelectedOptions2((prevState) =>
       prevState.includes(option)
         ? prevState.filter((item) => item !== option)
         : [...prevState, option]
@@ -21,7 +40,9 @@ export const Pricing = () => {
   };
 
   const handleButtonClick = () => {
-    const message = `Hola buenas, estoy interesado en estos servicios para mi negocio. Quiero más información.\n\n${selectedOptions
+    const message = `Hola buenas, estoy interesado en estos servicios para mi negocio. Quiero más información.\n\nServicios de Tecnología:\n${selectedOptions1
+      .map((option) => `- ${option}`)
+      .join("\n")}\n\nServicios de Multimedia:\n${selectedOptions2
       .map((option) => `- ${option}`)
       .join("\n")}\n\nMuchas gracias.`;
     const whatsappUrl = `https://api.whatsapp.com/send?phone=63536621&text=${encodeURIComponent(
@@ -30,26 +51,16 @@ export const Pricing = () => {
     window.open(whatsappUrl, "_blank");
   };
 
-  // Calculate the total price based on selected options
-  const totalPrice = optionsData
-    .filter((option) => selectedOptions.includes(option.text))
+  const totalPrice1 = optionsData1
+    .filter((option) => selectedOptions1.includes(option.text))
     .reduce((acc, option) => acc + option.price, 0);
 
-  // Determine the description based on the number of selected options
-  let description;
-  const numOptions = selectedOptions.length;
-  if (numOptions >= 5) {
-    description = "¡Super bien, has elegido un plan completo y versatil!";
-  } else if (numOptions >= 3) {
-    description = "¡Excelente, un par de opciones es lo ideal!";
-  } else if (numOptions >= 1) {
-    description = "¡Genial, eso es perfecto para comenzar!";
-  } else {
-    description = "Selecciona las opciones que más te convengan.";
-  }
+  const totalPrice2 = optionsData2
+    .filter((option) => selectedOptions2.includes(option.text))
+    .reduce((acc, option) => acc + option.price, 0);
 
   return (
-    <section className="w-screen flex justify-center bg-bgDark2 relative">
+    <section className="w-screen flex flex-col items-center bg-bgDark2 relative">
       <div className="absolute -top-16" id="pricing" />
       <div className="pb-20 pt-12 bg-bgDark2 2xl:w-[1150px] lg:w-[1050px] md:w-4/5">
         <motion.div
@@ -71,40 +82,90 @@ export const Pricing = () => {
               </p>
             </div>
 
-            <div className="-mx-4 items-center mt-20">
+            <div className="flex flex-wrap justify-center items-center gap-8 mt-20">
               <div className="px-4 mb-8 lg:mb-0 flex justify-center">
-                <div className="px-6 py-6 sm:px-8 sm:py-8 bg-bgDark3 rounded-3xl inline-block max-w-md sm:max-w-lg">
+                <div className="px-6 py-6 sm:px-8 pt-14 bg-bgDark3 rounded-3xl inline-block max-w-md sm:max-w-lg">
+                  <img
+                    src={pimenta_multimedia.src}
+                    alt="Imagen del plan 1"
+                    className="mx-auto mb-4"
+                  />
                   <div className="text-3xl sm:text-4xl font-bold text-primaryText text-center mt-4">
-                    Precio: ${totalPrice}
+                    Precio: ${totalPrice1}
                   </div>
                   <p className="mt-4 mb-6 text-gray-500 leading-loose text-center">
-                    {description}
+                    {selectedOptions1.length >= 5
+                      ? "¡Super bien, has elegido un plan completo y versatil!"
+                      : selectedOptions1.length >= 3
+                      ? "¡Excelente, un par de opciones es lo ideal!"
+                      : selectedOptions1.length >= 1
+                      ? "¡Genial, eso es perfecto para comenzar!"
+                      : "Selecciona las opciones que más te convengan."}
                   </p>
-                  <ul className="mb-2 sm:mb-6 text-primaryText text-left">
-                    {optionsData.map((option, index) => (
+                  <ul className="mb-8 text-primaryText text-left">
+                    {optionsData1.map((option, index) => (
                       <li
                         className="mb-4 flex items-center justify-start"
                         key={`${option.text}-${index}`}
                       >
                         <input
                           type="checkbox"
-                          checked={selectedOptions.includes(option.text)}
-                          onChange={() => handleOptionChange(option.text)}
+                          checked={selectedOptions1.includes(option.text)}
+                          onChange={() => handleOptionChange1(option.text)}
                           className="mr-2 w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
                         />
                         <span>{option.text}</span>
                       </li>
                     ))}
                   </ul>
-                  <button
-                    onClick={handleButtonClick}
-                    className="inline-block text-center py-2 px-4 rounded-xl contained-button font-bold leading-loose mt-8 sm:mt-16 m-auto"
-                    aria-label="Get started"
-                  >
-                    ¡Quiero este!
-                  </button>
                 </div>
               </div>
+              <div className="px-4 mb-8 lg:mb-0 flex justify-center">
+                <div className="px-6 py-6 sm:px-8 pt-14 bg-bgDark3 rounded-3xl inline-block max-w-md sm:max-w-lg">
+                  <img
+                    src={pimenta_informatica.src}
+                    alt="Imagen del plan 2"
+                    className="mx-auto mb-4"
+                  />
+                  <div className="text-3xl sm:text-4xl font-bold text-primaryText text-center mt-4">
+                    Precio: ${totalPrice2}
+                  </div>
+                  <p className="mt-4 mb-6 text-gray-500 leading-loose text-center">
+                    {selectedOptions2.length >= 5
+                      ? "¡Super bien, has elegido un plan completo y versatil!"
+                      : selectedOptions2.length >= 3
+                      ? "¡Excelente, un par de opciones es lo ideal!"
+                      : selectedOptions2.length >= 1
+                      ? "¡Genial, eso es perfecto para comenzar!"
+                      : "Selecciona las opciones que más te convengan."}
+                  </p>
+                  <ul className="mb-8 text-primaryText text-left">
+                    {optionsData2.map((option, index) => (
+                      <li
+                        className="mb-4 flex items-center justify-start"
+                        key={`${option.text}-${index}`}
+                      >
+                        <input
+                          type="checkbox"
+                          checked={selectedOptions2.includes(option.text)}
+                          onChange={() => handleOptionChange2(option.text)}
+                          className="mr-2 w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+                        />
+                        <span>{option.text}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 flex justify-center">
+              <button
+                onClick={handleButtonClick}
+                className="inline-block text-center py-2 px-4 rounded-xl contained-button font-bold leading-loose"
+                aria-label="Get started"
+              >
+                ¡Quiero Cotizar!
+              </button>
             </div>
           </div>
         </motion.div>
